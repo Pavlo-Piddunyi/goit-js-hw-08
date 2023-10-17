09496ce80a5d9ded1a6549872bafa56f8a238f1c
Pavlo-Piddunyi <135760789+Pavlo-Piddunyi@users.noreply.github.com>
2023-10-17 11:09:19 +0000

    Deploying to gh-pages from @ Pavlo-Piddunyi/goit-js-hw-08@a3c725630542d027b925e28bcf536c7580f9ba07 🚀

--- index.cdaba638.js
+++
@@ -1,2 +0,0 @@
-!function(){console.log(10);console.log(20)}();
-//# sourceMappingURL=index.cdaba638.js.map
--- index.dcc2f779.js
+++
@@ -1,2 +0,0 @@
-console.log(10);console.log(20);
-//# sourceMappingURL=index.dcc2f779.js.map
