3682a9c0cbd2b68dd64ec2b24f287b509117188a
Pavlo-Piddunyi <135760789+Pavlo-Piddunyi@users.noreply.github.com>
2023-10-18 15:36:48 +0300

    TASK-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,41 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
 
-const gallery = document.querySelector(".gallery")
-const item = galleryItems.map(({ preview, original, description }) => {
+const galleryList = document.querySelector('.gallery')
+const markup = galleryItems.map(({ preview, original, description }) => {
     return `<li class="gallery__item">
   <a class="gallery__link"  href="${original}">
     <img class="gallery__image"
       loading="lazy"
       src="${preview}"
-      data-source="${original}"
-      alt="${description}"
+      title="${description}"
     />
   </a>
 </li>`;
-}).join("");
-gallery.insertAdjacentHTML("beforeend", item)
-gallery.addEventListener("click", imageClick)
-function imageClick(evt) {
-    noLink(evt)
-    if (evt.target.nodeName !== "IMG") {
-        return;
-    }
-    const source = evt.target.dataset.source;
-    // console.log(source)
-    const discribe = evt.target.getAttribute('alt');
-    // console.dir(discribe)
-    const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" alt="${discribe}">`);
-    instance.show();
-    gallery.addEventListener("keydown", (evt) => {
-        if (evt.code === "Escape") {
-            instance.close()
-        }
-    })
-}
-function noLink(evt) {
-     evt.preventDefault();
-}
+}).join('');
+
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true, 
+    captionDelay: 250,
+    fadeSpeed: 250,
+    captionSelector: 'img',
+    captionData: 'alt',
+    captionPosition: 'bottom',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe')
+const vimeoPlayer = new Player(iframe);
+
+const onPlay = function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+vimeoPlayer.on('timeupdate', throttle(onPlay, 1000));
+
+const carrentTime = Number(localStorage.getItem('videoplayer-current-time'))
+
+vimeoPlayer.setCurrentTime(carrentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
+});
